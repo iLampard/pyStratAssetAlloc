9687fdf53f5d38e01d300366087de9cb5289b9d3
iLampard <siqiao_xue@163.com>
2017-04-04 12:36:15 +0800

    Remove .idea files

--- pyStratAssetAlloc/technical/example/llt_hs300.py
+++ pyStratAssetAlloc/technical/example/llt_hs300.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     data = get_sec_price('2016-01-01', '2017-01-01', ['000300.SH'], DataSource.WIND)  # 获取2016的HS300收盘数据
-    ts = LLT(alpha=2.0 / 61.0, dependency='000300.SH')  # 定义llt模块
+    ts = LLT(alpha=2.0/61.0, dependency='000300.SH')  # 定义llt模块
     res = ts.transform(data)  # 获取HS300的llt序列
     plt.xlabel("date")
     plt.ylabel("index")
