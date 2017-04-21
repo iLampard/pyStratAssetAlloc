9f417c71b4d8a7c250af1f5deafd84d11688c596
iLampard <siqiao_xue@163.com>
2017-04-21 16:38:57 +0800

    Use future as underlying in CTA strat

--- pyStratAssetAlloc/strat/CTA/dual_thrust.py
+++ pyStratAssetAlloc/strat/CTA/dual_thrust.py
@@ -13,9 +13,8 @@ from PyFin.api import HIGH
 from PyFin.api import LOW
 from PyFin.api import CLOSE
 from decouple import config
+from pyStratAssetAlloc.utils import get_continuous_future_contract
 
-
-#TODO use continuse futures as underlying
 
 class DualThrust(Strategy):
     def __init__(self, window_up, multi_up, window_down, multi_down, **kwargs):
@@ -32,31 +31,30 @@ class DualThrust(Strategy):
         self._day_count = 0
         self._update_date = None
         self._save_thrust = kwargs.get('save_thrust', True)
+        self._del_rule = kwargs.get('del_rule', {'nth': 3, 'day_of_week': 6})
+        self._main_contract = None
 
     def handle_data(self):
         self._update_thrust()
-        for sec in self.universe:
-            if len(self._thrust[sec]['thrust_up']) == 0 or len(self._thrust[sec]['thrust_down']) == 0:
-                return
-            signal = 1 if self._close[sec] > self._thrust[sec]['thrust_up'][-1] else -1 if self._close[sec] < \
-                                                                                           self._thrust[sec][
-                                                                                               'thrust_down'][
-                                                                                               -1] else 0
-            if signal > 0 >= self.secPos[sec]:
-                self.order_to_pct(sec, 1, 1)
-            elif signal < 0 <= self.secPos[sec]:
-                self.order_to_pct(sec, -1, 1)
+        current_date = self.current_datetime
+        fut_id = get_continuous_future_contract(self.universe, current_date, self._del_rule)
+        if fut_id != self._main_contract and self.secPos[self._main_contract] != 0:
+            self.order_to(self._main_contract, 1, 0)
+
+        if len(self._thrust[fut_id]['thrust_up']) == 0 or len(self._thrust[fut_id]['thrust_down']) == 0:
+            return
+        signal = 1 if self._close[fut_id] > self._thrust[fut_id]['thrust_up'][-1] else -1 \
+            if self._close[fut_id] < self._thrust[fut_id]['thrust_down'][-1] else 0
+        if signal > 0 >= self.secPos[fut_id]:
+            self.order_to(fut_id, 1, 1)
+        elif signal < 0 <= self.secPos[fut_id]:
+            self.order_to(fut_id, -1, 1)
 
     def _update_thrust(self):
         if self._update_date != self.current_date:  # 开始新的一天
             self._append_price()
             self._update_date = self.current_date
-            self._day_count += 1
-
-            if self._day_count <= max(self._window_up, self._window_down):
-                return
-            else:
-                self._calc_thrust()
+            self._calc_thrust()
 
         elif self._update_date == self.current_date:  # 如果还在同一天
             self._update_price_daily_on_same_day()
@@ -90,24 +88,27 @@ class DualThrust(Strategy):
         lc = min(self._price_daily[sec]['close'][-window:-1])
         # N日low的最低价
         ll = min(self._price_daily[sec]['low'][-window:-1])
+
         calc_range = max(hh - lc, hc - ll)
         return calc_range
 
     def _calc_thrust(self):
         for sec in self.universe:
-            thrust_up = self._open[sec] + self._multi_up * self._calc_range(sec, self._window_up)
-            thrust_down = self._open[sec] - self._multi_down * self._calc_range(sec, self._window_down)
-            self._thrust[sec]['thrust_up'].append(thrust_up)
-            self._thrust[sec]['thrust_down'].append(thrust_down)
-            if self._save_thrust:
-                self.keep('thrust_up_' + sec, thrust_up)
-                self.keep('thrust_down_' + sec, thrust_down)
+            if len(self._price_daily[sec]['high']) >= self._window_up + 1:
+                thrust_up = self._open[sec] + self._multi_up * self._calc_range(sec, self._window_up)
+                thrust_down = self._open[sec] - self._multi_down * self._calc_range(sec, self._window_down)
+                self._thrust[sec]['thrust_up'].append(thrust_up)
+                self._thrust[sec]['thrust_down'].append(thrust_down)
+                if self._save_thrust:
+                    self.keep('thrust_up_' + sec, thrust_up)
+                    self.keep('thrust_down_' + sec, thrust_down)
 
 
 def run_example():
-    universe = ['000905.zicn']
-    start_date = dt.datetime(2015, 12, 10)
-    end_date = dt.datetime(2016, 4, 1)
+    # universe = ['if15%02d.ccfx' % i for i in range(1, 13)]
+    universe = ['if15%02d.ccfx' % i for i in range(10, 13)] + ['if16%02d.ccfx' % i for i in range(1, 4)]
+    start_date = dt.datetime(2015, 11, 10)
+    end_date = dt.datetime(2016, 2, 1)
 
     window_up = config('DT_WINDOW_UP', cast=float)
     multi_up = config('DT_MULTI_UP', cast=float)
@@ -119,13 +120,12 @@ def run_example():
                    symbolList=universe,
                    startDate=start_date,
                    endDate=end_date,
-                   benchmark='000905.zicn',
+                   benchmark='000300.zicn',
                    dataSource=DataSource.WIND,
                    logLevel='info',
                    saveFile=True,
-                   portfolioType=PortfolioType.CashManageable,
                    plot=True,
-                   freq='D')
+                   freq='min5')
 
 
 if __name__ == "__main__":
--- pyStratAssetAlloc/utils/__init__.py
+++ pyStratAssetAlloc/utils/__init__.py
@@ -2,6 +2,8 @@
 
 from pyStratAssetAlloc.utils.misc import get_sec_price
 from pyStratAssetAlloc.utils.misc import find_in_interval
+from pyStratAssetAlloc.utils.misc import get_continuous_future_contract
 
 __all__ = ['get_sec_price',
-           'map_to_interval']
+           'map_to_interval',
+           'get_continuous_future_contract']
